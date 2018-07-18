c65abd8bc559ec7062471c62adecabcb8224ce92
curtischatch <curtis.chat.hk@gmail.com>
2018-07-18 14:04:23 -0400

    exercise0

---
+++ exercise.rb
@@ -0,0 +1,6 @@
+#0
+fav_colours = ["blue","black","green"]
+the_ages = [23,26,35,23]
+coin_toss = ["heads","heads","heads","tails","heads" ]
+fav_artists = ["DPR","Toki","Taku","Anders"]
+fav_colours = [:blue,:black,:green]
